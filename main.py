5f9d739e9007a7723fd313ed2c7cfb943df72eae
COLD <66121689+cold9999@users.noreply.github.com>
2023-09-01 16:42:59 +0900

    9/1

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ screen = pygame.display.set_mode((540, 720))
 clock = pygame.time.Clock()
 running = True
 background = pygame.image.load(os.path.join("images","background.jpg"))
-
+o_img = pygame.image.load(os.path.join("images","O.png"))
+x_img = pygame.image.load(os.path.join("images","X.png"))
 screen.blit(background,(0,0))
 #박스 생성
 main_box = pygame.Rect(90,200,360,360)
@@ -15,6 +16,7 @@ pygame.draw.line(screen,"black",pygame.Vector2(90,320),pygame.Vector2(450,320),w
 pygame.draw.line(screen,"black",pygame.Vector2(90,440),pygame.Vector2(450,440),width=3)#가로 2
 pygame.draw.line(screen,"black",pygame.Vector2(210,200),pygame.Vector2(210,560),width=3)#세로 3
 pygame.draw.line(screen,"black",pygame.Vector2(330,200),pygame.Vector2(330,560),width=3)#세로 4
+
 button1 = pygame.Rect(95,205,110,110)
 button2 = pygame.Rect(215,205,110,110)
 button3 = pygame.Rect(335,205,110,110)
@@ -24,15 +26,15 @@ button6 = pygame.Rect(335,325,110,110)
 button7 = pygame.Rect(95,445,110,110)
 button8 = pygame.Rect(215,445,110,110)
 button9 = pygame.Rect(335,445,110,110)
-pygame.draw.rect(screen,"black",button1,0,10)
-pygame.draw.rect(screen,"black",button2,0,10)
-pygame.draw.rect(screen,"black",button3,0,10)
-pygame.draw.rect(screen,"black",button4,0,10)
-pygame.draw.rect(screen,"black",button5,0,10)
-pygame.draw.rect(screen,"black",button6,0,10)
-pygame.draw.rect(screen,"black",button7,0,10)
-pygame.draw.rect(screen,"black",button8,0,10)
-pygame.draw.rect(screen,"black",button9,0,10)
+pygame.draw.rect(screen,"white",button1,0,10)
+pygame.draw.rect(screen,"white",button2,0,10)
+pygame.draw.rect(screen,"white",button3,0,10)
+pygame.draw.rect(screen,"white",button4,0,10)
+pygame.draw.rect(screen,"white",button5,0,10)
+pygame.draw.rect(screen,"white",button6,0,10)
+pygame.draw.rect(screen,"white",button7,0,10)
+pygame.draw.rect(screen,"white",button8,0,10)
+pygame.draw.rect(screen,"white",button9,0,10)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,7 +42,23 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if button1.collidepoint(event.pos):
-                    pass # 클릭시 작업
+                    screen.blit(o_img,(95,205))
+                if button2.collidepoint(event.pos):
+                    screen.blit(o_img,(215,205))
+                if button3.collidepoint(event.pos):
+                    screen.blit(o_img,(335,205))
+                if button4.collidepoint(event.pos):
+                    screen.blit(o_img,(95,325))
+                if button5.collidepoint(event.pos):
+                    screen.blit(o_img,(215,325))
+                if button6.collidepoint(event.pos):
+                    screen.blit(o_img,(335,325))
+                if button7.collidepoint(event.pos):
+                    screen.blit(o_img,(95,445))
+                if button8.collidepoint(event.pos):
+                    screen.blit(o_img,(215,445))
+                if button9.collidepoint(event.pos):
+                    screen.blit(o_img,(335,445))
     pygame.display.flip()
     clock.tick(60)
 
