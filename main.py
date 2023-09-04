966a6d08913ed5f38c748753e312c9839bcab7ae
cold9999 <neutronsycth@gmail.com>
2023-09-04 11:56:21 +0900

    9/4

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ o_img = pygame.image.load(os.path.join("images","O.png"))
 x_img = pygame.image.load(os.path.join("images","X.png"))
 screen.blit(background,(0,0))
 #게임 플레이 변수
-pressed_list = {"1":False,"2":False,"3":False,"4":False,"5":False,"6":False,"7":False,"8":False,"9":False}
+#0이 지정 X, False가 컴터, True가 플레이어
+pressed_list = {"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0}
 button_sizes = {"1":(95,205),"2":(215,205),"3":(335,205),"4":(95,325),"5":(215,325),"6":(335,325),"7":(95,445),"8":(215,445),"9":(335,445)}
 #박스 생성
 button1 = pygame.Rect(95,205,110,110)
@@ -24,7 +25,6 @@ button8 = pygame.Rect(215,445,110,110)
 button9 = pygame.Rect(335,445,110,110)
 button_reset = pygame.Rect(50,50,60,60)
 pygame.draw.rect(screen,"white",button_reset,0,10)
-turn_num = 0
 def reset():
     global pressed_list, turn_num
     main_box = pygame.Rect(90,200,360,360)
@@ -52,24 +52,28 @@ def reset():
     pygame.draw.rect(screen,"white",button7,0,10)
     pygame.draw.rect(screen,"white",button8,0,10)
     pygame.draw.rect(screen,"white",button9,0,10)
-    pressed_list = {"1":False,"2":False,"3":False,"4":False,"5":False,"6":False,"7":False,"8":False,"9":False}
-    turn_num = 0
+    pressed_list = {"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0}
+def get_danger():
+    print(list(pressed_list.items())[1])
+get_danger()
 def com_play(arg:str):
     global turn_num
-    turn_num += 1
     if arg == "5":
         while True:
             ran_sel = random.choice(["2","4","6","8"])
-            if pressed_list[ran_sel] == False:
+            if pressed_list[ran_sel] == 0:
                 screen.blit(x_img,button_sizes[ran_sel])
-                pressed_list[ran_sel] = True
+                pressed_list[ran_sel] = False
                 break
             else:
                 continue
     else:
-        if pressed_list["5"] == False:
+        #앞에 위험 체크해야됨
+        if pressed_list["5"] == 0:
             screen.blit(x_img,button_sizes["5"])
-            pressed_list["5"] = True
+            pressed_list["5"] = False
+        else:
+            pass
 reset()
 while running:
     for event in pygame.event.get():
@@ -78,47 +82,47 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if button1.collidepoint(event.pos):
-                    if pressed_list["1"] == False:
+                    if pressed_list["1"] == 0:
                         screen.blit(o_img,button_sizes["1"])
                         pressed_list["1"] = True
                         com_play("1")
                 if button2.collidepoint(event.pos):
-                    if pressed_list["2"] == False:
+                    if pressed_list["2"] == 0:
                         screen.blit(o_img,button_sizes["2"])
                         pressed_list["2"] = True
                         com_play("2")
                 if button3.collidepoint(event.pos):
-                    if pressed_list["3"] == False:
+                    if pressed_list["3"] == 0:
                         screen.blit(o_img,button_sizes["3"])
                         pressed_list["3"] = True
                         com_play("3")
                 if button4.collidepoint(event.pos):
-                    if pressed_list["4"] == False:
+                    if pressed_list["4"] == 0:
                         screen.blit(o_img,button_sizes["4"])
                         pressed_list["4"] = True
                         com_play("4")
                 if button5.collidepoint(event.pos):
-                    if pressed_list["5"] == False:
+                    if pressed_list["5"] == 0:
                         screen.blit(o_img,button_sizes["5"])
                         pressed_list["5"] = True
                         com_play("5")
                 if button6.collidepoint(event.pos):
-                    if pressed_list["6"] == False:
+                    if pressed_list["6"] == 0:
                         screen.blit(o_img,button_sizes["6"])
                         pressed_list["6"] = True
                         com_play("6")
                 if button7.collidepoint(event.pos):
-                    if pressed_list["7"] == False:
+                    if pressed_list["7"] == 0:
                         screen.blit(o_img,button_sizes["7"])
                         pressed_list["7"] = True
                         com_play("7")
                 if button8.collidepoint(event.pos):
-                    if pressed_list["8"] == False:
+                    if pressed_list["8"] == 0:
                         screen.blit(o_img,button_sizes["8"])
                         pressed_list["8"] = True
                         com_play("8")
                 if button9.collidepoint(event.pos):
-                    if pressed_list["9"] == False:
+                    if pressed_list["9"] == 0:
                         screen.blit(o_img,button_sizes["9"])
                         pressed_list["9"] = True
                         com_play("9")
